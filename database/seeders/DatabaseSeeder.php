6f77c9f183b048b695b41153d1fbb3b9df20ec18
Himaryy <122810667+Himaryy@users.noreply.github.com>
2025-02-06 18:30:15 +0700

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Himary',
+            'email' => 'himary@example.com',
+            'password' => bcrypt('123qweasd'),
+            'email_verified_at' => time(),
+        ]);
+
+        Project::Factory()->count(30)->hasTasks(30)->create();
+    }
+}
